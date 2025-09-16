3fcc390152350aaba2a9efc05656c1e7a56258d8
aref abolhasani <arefabolhasani21@gmail.com>
2025-09-17 02:23:09 +0330

    feat: Initial project setup

---
+++ mylibrary/library.py
@@ -0,0 +1,15 @@
+class Library:
+    def __init__(self):
+         self.boks = []
+
+    def add_books(self, title, author):
+         pass
+
+    def remove_book(self, title):
+        pass
+
+    def search_book(self, title):
+        pass
+
+    def show_books(self):
+       return self.books
